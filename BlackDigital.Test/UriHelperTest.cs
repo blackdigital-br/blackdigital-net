914ba065f18454542db87b8155aa1990dbc1b9df
Ricardo Coelho <tiorac@msn.com>
2023-01-05 13:02:20 -0300

    uriHelper

---
+++ BlackDigital.Test/UriHelperTest.cs
@@ -0,0 +1,29 @@
+﻿
+namespace BlackDigital.Test
+{
+    public class UriHelperTest
+    {
+        private static readonly string UrlTest = "https://blackdigital.com.br/test?txt=my%20Text&nmb=10&myDate=2023-01-01";
+
+        [Fact]
+        public void AppendUri()
+        {
+            Uri uri = new(UrlTest);
+            uri = uri.Append("test2", "test3");
+
+            Assert.Equal("https://blackdigital.com.br/test/test2/test3?txt=my%20Text&nmb=10&myDate=2023-01-01", uri.AbsoluteUri);
+        }
+
+        [Fact]
+        public void GetQueryString()
+        {
+            Uri uri = new(UrlTest);
+
+            var queryString = uri.GetQueryString();
+
+            Assert.Equal("my%20Text", queryString["txt"]);
+            Assert.Equal("10", queryString["nmb"]);
+            Assert.Equal("2023-01-01", queryString["myDate"]);
+        }
+    }
+}
---
+++ BlackDigital/UriHelper.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace BlackDigital
+{
+    public static class UriHelper
+    {
+        public static Uri Append(this Uri uri, params string[] paths)
+        {
+            string path = String.Format("{0}{1}{2}{3}", uri.Scheme,
+                Uri.SchemeDelimiter, uri.Authority, uri.AbsolutePath);
+
+            Uri newUri = new(path);
+
+            newUri = new Uri(paths.Aggregate(newUri.AbsoluteUri, (current, path) => string.Format("{0}/{1}", current.TrimEnd('/'), path.TrimStart('/'))));
+
+            UriBuilder builder = new(newUri);
+            builder.Query = uri.Query;
+
+            return builder.Uri;
+        }
+
+        public static Dictionary<string, string> GetQueryString(this Uri uri)
+        {
+            if (uri == null || string.IsNullOrWhiteSpace(uri.Query))
+                return new();
+
+            var query = uri.Query.Substring(1);
+
+            return query.Split('&')
+                            .ToDictionary(q => q.Split('=')[0], q => q.Split('=')[1]);
+        }
+    }
+}
