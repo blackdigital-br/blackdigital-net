f62afa0c9584d0ac2d4c28062b5d3787306a6202
Ricardo Coelho <tiorac@msn.com>
2023-03-03 23:47:38 -0300

    Reflection Helper

---
+++ src/ReflectionHelper.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Reflection;
+
+namespace BlackDigital
+{
+    public static class ReflectionHelper
+    {
+        /*public static TAttribute[] GetAttributes<TAttribute>(this Assembly assembly)
+            where TAttribute : Attribute =>
+                assembly.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .ToArray();
+
+        public static TAttribute? GetSingleAttribute<TAttribute>(this Assembly assembly)
+            where TAttribute : Attribute =>
+                assembly.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .SingleOrDefault();*/
+
+        public static TAttribute[] GetAttributes<TAttribute>(this Type type)
+            where TAttribute : Attribute =>
+                type.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .ToArray();
+
+
+        public static TAttribute? GetSingleAttribute<TAttribute>(this Type type)
+            where TAttribute : Attribute =>
+                type.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .SingleOrDefault();
+
+        public static TAttribute[] GetAttributes<TAttribute>(this MethodInfo method)
+            where TAttribute : Attribute =>
+                method.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .ToArray();
+
+
+        public static TAttribute? GetSingleAttribute<TAttribute>(this MethodInfo method)
+            where TAttribute : Attribute =>
+                method.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .SingleOrDefault();
+
+        public static TAttribute[] GetAttributes<TAttribute>(this PropertyInfo property)
+            where TAttribute : Attribute =>
+                property.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .ToArray();
+
+
+        public static TAttribute? GetSingleAttribute<TAttribute>(this PropertyInfo property)
+            where TAttribute : Attribute =>
+                property.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .SingleOrDefault();
+
+        public static TAttribute[] GetAttributes<TAttribute>(this MemberInfo member)
+            where TAttribute : Attribute =>
+                member.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .ToArray();
+
+
+        public static TAttribute? GetSingleAttribute<TAttribute>(this MemberInfo member)
+            where TAttribute : Attribute =>
+                member.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .SingleOrDefault();
+
+        public static TAttribute[] GetAttributes<TAttribute>(this ParameterInfo parameter)
+            where TAttribute : Attribute =>
+                parameter.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .ToArray();
+
+        public static TAttribute? GetSingleAttribute<TAttribute>(this ParameterInfo parameter)
+            where TAttribute : Attribute =>
+                parameter.GetCustomAttributes(typeof(TAttribute), true)
+                        .Cast<TAttribute>()
+                        .SingleOrDefault();
+
+
+    }
+}
---
+++ test/ReflectionHelperTest.cs
@@ -0,0 +1,74 @@
+﻿using BlackDigital.Rest;
+using BlackDigital.Test.Mock;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlackDigital.Test
+{
+    public class ReflectionHelperTest
+    {
+        [Fact]
+        public void GetAttributeFromType()
+        {
+            var attributes = typeof(IServiceTest).GetAttributes<ServiceAttribute>();
+
+            Assert.Single(attributes);
+            Assert.Equal(typeof(ServiceAttribute), attributes.First().GetType());
+
+            var attribute = typeof(IServiceTest).GetSingleAttribute<ServiceAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal(typeof(ServiceAttribute), attribute.GetType());
+        }
+
+        [Fact]
+        public void GetAttributeFromMethod()
+        {
+            var attributes = typeof(IServiceTest).GetMethod("MyAction").GetAttributes<ActionAttribute>();
+
+            Assert.Single(attributes);
+            Assert.Equal(typeof(ActionAttribute), attributes.First().GetType());
+
+            var attribute = typeof(IServiceTest).GetMethod("MyAction").GetSingleAttribute<ActionAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal(typeof(ActionAttribute), attribute.GetType());
+        }
+
+        [Fact]
+        public void GetAttributeFromProperty()
+        {
+            var attributes = typeof(SimpleClass).GetProperty("Id").GetAttributes<KeyAttribute>();
+
+            Assert.Single(attributes);
+            Assert.Equal(typeof(KeyAttribute), attributes.First().GetType());
+
+            var attribute = typeof(SimpleClass).GetProperty("Id").GetSingleAttribute<KeyAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal(typeof(KeyAttribute), attribute.GetType());
+        }
+
+        [Fact]
+        public void GetAttributeFromMember()
+        {
+            var attributes = typeof(SimpleClass).GetMember("Name").First().GetAttributes<RequiredAttribute>();
+
+            Assert.Single(attributes);
+            Assert.Equal(typeof(RequiredAttribute), attributes.First().GetType());
+
+            var attribute = typeof(SimpleClass).GetMember("Name").First().GetSingleAttribute<RequiredAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal(typeof(RequiredAttribute), attribute.GetType());
+        }
+
+        [Fact]
+        public void GetAttributeFromParameter()
+        {
+            var attributes = typeof(IServiceTest).GetMethod("MyAction").GetParameters().First().GetAttributes<FromRouteAttribute>();
+
+            Assert.Single(attributes);
+            Assert.Equal(typeof(FromRouteAttribute), attributes.First().GetType());
+
+            var attribute = typeof(IServiceTest).GetMethod("MyAction").GetParameters().First().GetSingleAttribute<FromRouteAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal(typeof(FromRouteAttribute), attribute.GetType());
+        }
+    }
+}
