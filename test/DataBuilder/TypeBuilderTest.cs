9410abb3e83e35c0c15245157974e84273229ef4
Ricardo Coelho <tiorac@msn.com>
2023-04-05 00:31:14 -0300

    Unit Test

---
+++ test/DataBuilder/TypeBuilderTest.cs
@@ -0,0 +1,22 @@
+﻿using BlackDigital.DataBuilder;
+using BlackDigital.Test.Mock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackDigital.Test.DataBuilder
+{
+    public class TypeBuilderTest
+    {
+        [Fact]
+        public void CreateTypeBuilder()
+        {
+            var typeBuilder = TypeBuilder.Create<SimpleClass>();
+
+            Assert.Equal(1, typeBuilder?.Properties.Count);
+            
+        }
+    }
+}
