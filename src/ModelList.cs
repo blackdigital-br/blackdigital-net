779d718fd09fa917497090433f566a0a8c320f90
Ricardo Coelho <tiorac@msn.com>
2023-04-15 14:56:57 -0300

    ModelList

---
+++ src/ModelList.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BlackDigital
+{
+    public record ModelList<T>
+        where T : class
+    {
+        public IEnumerable<T> Itens { get; init; }
+
+        public int TotalItens { get; init; }
+    }
+}
