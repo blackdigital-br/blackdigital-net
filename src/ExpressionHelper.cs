b7041b2292123f343099de6594d130a0e4f8151f
Ricardo Coelho <tiorac@msn.com>
2025-06-04 10:15:14 -0300

    Adicionar método para extrair propriedades de expressões
    Implementado o método `GetPropertiesInfoFromExpression` na classe `ExpressionHelper`, que permite extrair informações de propriedades a partir de expressões, suportando tanto múltiplas quanto únicas propriedades. Adicionados testes unitários na classe `ExpressionHelperTest` para validar o funcionamento do novo método.

--- src/ExpressionHelper.cs
+++ src/ExpressionHelper.cs
@@ -22,5 +22,34 @@ namespace BlackDigital
 
             return query.Where(lambda);
         }
+
+        public static IEnumerable<PropertyInfo> GetPropertiesInfoFromExpression<TModel>(this Expression<Func<TModel, object>>? expression)
+        {
+            if (expression == null)
+                yield break;
+
+            if (expression.Body is NewExpression newExpr)
+            {
+                foreach (var arg in newExpr.Arguments)
+                {
+                    if (arg is MemberExpression memberExpr && memberExpr.Member is PropertyInfo propInfo)
+                        yield return propInfo;
+                    else
+                        throw new InvalidOperationException("All arguments in the expression must be properties.");
+                }
+            }
+            else if (expression.Body is MemberExpression memberExprSingle && memberExprSingle.Member is PropertyInfo propInfoSingle)
+            {
+                yield return propInfoSingle;
+            }
+            else if (expression.Body is UnaryExpression unaryExpr && unaryExpr.Operand is MemberExpression memberExprUnary && memberExprUnary.Member is PropertyInfo propInfoUnary)
+            {
+                yield return propInfoUnary;
+            }
+            else
+            {
+                throw new InvalidOperationException("IgnoreProperties expression must be a NewExpression.");
+            }
+        }
     }
 }
---
+++ test/ExpressionHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using BlackDigital.Test.Mock;
+
+
+namespace BlackDigital.Test
+{
+    public class ExpressionHelperTest
+    {
+        [Fact(DisplayName = "Get Multiple Properties From Expression")]
+        public void GetMonthFromMidDate()
+        {
+            var properties = ExpressionHelper.GetPropertiesInfoFromExpression<ComplexModel>(x => new { x.Id, x.Name });
+
+            Assert.NotNull(properties);
+            Assert.Equal(2, properties.Count());
+            Assert.Contains(properties, p => p.Name == "Id");
+            Assert.Contains(properties, p => p.Name == "Name");
+        }
+
+        [Fact(DisplayName = "Get Single Property From Expression")]
+        public void GetSinglePropertyFromExpression()
+        {
+            var properties = ExpressionHelper.GetPropertiesInfoFromExpression<ComplexModel>(x => x.Id);
+            Assert.NotNull(properties);
+            Assert.Single(properties);
+            Assert.Contains(properties, p => p.Name == "Id");
+        }
+
+    }
+}
